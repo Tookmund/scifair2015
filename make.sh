7884df82db56237367de8752e860d84b71256d23
Jacob Adams <tookmund@gmail.com>
2015-12-16 22:14:46 -0500

    make: Make make smarter about making files
    :)

--- make.sh
+++ make.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # Make qemu machine and disk
-wget http://get.debian.org/cdimage/archive/7.9.0-live/i386/iso-hybrid/debian-live-7.9.0-i386-standard.iso -O debian.img
-sha512sum -c SHA512SUM
-qemu-img create -f qcow2 hda.img 5G
-qemu-system-i386 -cdrom debian*.iso -hda hda.img -m 512
+if [ ! -e debian.iso ]
+then
+	wget http://get.debian.org/cdimage/archive/7.9.0-live/i386/iso-hybrid/debian-live-7.9.0-i386-standard.iso -O debian.iso
+fi
+
+sha512sum -c SHA512SUMS
+
+if [ ! -e hda.img ]
+then 
+	qemu-img create -f qcow2 hda.img 5G
+fi
+qemu-system-i386 -cdrom debian.iso -hda hda.img -m 512
